45d4eba415a2fe1a146013c98f1249a32bfa6e04
Yin <yinnnn17@gmail.com>
2021-03-23 19:45:25 -0700

    improve solution for problem 1198

--- Find Smallest Common Element in All Rows.py
+++ Find Smallest Common Element in All Rows.py
@@ -5,7 +5,6 @@ class Solution:
     def smallestCommonElement(self, mat: List[List[int]]) -> int:
         all_rows = len(mat)
         common_elems = {}
-        all_rows_common_elems = []
         for i in range(0, all_rows):
             for j in range(0, len(mat[i])):
                 target = mat[i][j]
@@ -14,8 +13,5 @@ class Solution:
                 else:
                     common_elems[target] = 1
                 if common_elems[target] == all_rows:
-                    all_rows_common_elems.append(target)
-        if len(all_rows_common_elems) == 0:
-            return -1
-        else:
-            return all_rows_common_elems[0]
+                    return target
+        return -1
